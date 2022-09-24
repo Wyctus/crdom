bcaf69521be5a98dfefe648e0994cd7dc2da03a0
Wyctus <ujszaszi.zzoltan@gmail.com>
2022-09-24 07:53:09 +0200

    feat: re-exported modules

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import { DoubleLinkedListNode, DoubleLinkedList } from "./DoubleLinkedList";
+import { TreeNode, Tree } from "./Tree";
+import Document from "./Document/Document";
+import Block from "./Document/BlockData";
+import BlockInput from "./Document/BlockInput";
+
+export { DoubleLinkedListNode, DoubleLinkedList, TreeNode, Tree, Document, Block, BlockInput };
